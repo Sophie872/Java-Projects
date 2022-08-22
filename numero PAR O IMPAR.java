39db53f6508a688e413e1995ea0525fbaf377ff3
Lisbeth Mujica <76069886+Sophie872@users.noreply.github.com>
2022-08-22 17:37:38 -0400

    Update and rename numero PAR O IMPAR.class to numero PAR O IMPAR.java

---
+++ numero PAR O IMPAR.java
@@ -0,0 +1,13 @@
+int numero 7; if (numero>0){
+  }
+  if (numero %2==0)
+  {
+    system.out.println ("El numero introducido es Par");
+    }
+else
+{
+  System.ot.println ("El numero introducido es Impar");
+  }
+else
+{
+   System.ot.println ("Introduce algun numero positivo");
